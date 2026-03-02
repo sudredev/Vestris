6c608b96cf18711ebe2b92ce5e35df714cd445c8
sudredev <e.sudrenascimento@gmail.com>
2026-03-02 18:15:48 -0300

    Adicionando update da base via properties

--- infra/init-schemas.sql
+++ infra/init-schemas.sql
@@ -1,3 +1,11 @@
-
+CREATE SCHEMA IF NOT EXISTS species_schema;
+CREATE SCHEMA IF NOT EXISTS pharmacology_schema;
+CREATE SCHEMA IF NOT EXISTS clinical_schema;
+CREATE SCHEMA IF NOT EXISTS vaccination_schema;
+CREATE SCHEMA IF NOT EXISTS reference_schema;
+CREATE SCHEMA IF NOT EXISTS medical_record_schema;
+CREATE SCHEMA IF NOT EXISTS feedback_schema;
+CREATE SCHEMA IF NOT EXISTS saas_schema;
+CREATE SCHEMA IF NOT EXISTS users_schema; -- Garante que existe
 -- Conceder permissões (se necessário em prod, aqui é local)
 GRANT ALL PRIVILEGES ON DATABASE vestris_db TO vestris_user;
